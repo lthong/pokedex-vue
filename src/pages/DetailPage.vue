fe0c0b4b36b298fdbf7f4a19cb7a968362209909
lthong <appyegde58965@gmail.com>
2023-10-22 13:17:07 +0800

    resolve the web history issue

--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -6,43 +6,27 @@
         <div class="id">{{ `#${formatPokeId}` }}</div>
       </div>
       <div :class="['pokemon', { [mainType]: !!mainType }]">
-        <img
-          key="prePokeDetailBtn"
-          :class="['arrow-btn left', { disabled: preBtnDisabled }]"
-          :src="assets.arrowIcon"
-          alt="arrow-icon"
-          @click="onArrowBtnClick('pre')"
-        />
+        <img key="prePokeDetailBtn" :class="['arrow-btn left', { disabled: preBtnDisabled }]" :src="assets.arrowIcon"
+          alt="arrow-icon" @click="onArrowBtnClick('pre')" />
         <div class="data-box">
           <div class="block">
             <SaveMyPokeBtn :id="id" />
             <div class="img-container">
-              <img
-                class="poke-photo"
-                :src="`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatPokeId}.png`"
-                :alt="name"
-              />
+              <img class="poke-photo" :src="`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatPokeId}.png`"
+                :alt="name" />
             </div>
           </div>
           <div class="row">
             <div class="col-1-1">
               <div class="label">{{ $t('common.states') }}</div>
               <div class="content">
-                <div
-                  v-for="({ label, value }, index) in states"
-                  :key="`state-${index}`"
-                  class="state"
-                >
+                <div v-for="({ label, value }, index) in states" :key="`state-${index}`" class="state">
                   <div class="label">{{ $t(`common.${label}`) }}</div>
                   <div class="value">
-                    <div
-                      :class="['bar', { full: getBarWidth(value) === 100 }]"
-                      :style="
-                        isStatesBarReady
-                          ? { width: `${getBarWidth(value)}%` }
-                          : {}
-                      "
-                    />
+                    <div :class="['bar', { full: getBarWidth(value) === 100 }]" :style="isStatesBarReady
+                      ? { width: `${getBarWidth(value)}%` }
+                      : {}
+                      " />
                     <span>{{ `${value} / 300` }}</span>
                   </div>
                 </div>
@@ -69,11 +53,7 @@
             <div class="col-1-1">
               <div class="label">{{ $t('common.types') }}</div>
               <div class="content">
-                <PokemonType
-                  v-for="(item) in basicInfo.types"
-                  :key="item.type.name"
-                  :type="item.type.name"
-                />
+                <PokemonType v-for="(item) in basicInfo.types" :key="item.type.name" :type="item.type.name" />
               </div>
             </div>
           </div>
@@ -92,13 +72,8 @@
             </div>
           </div>
         </div>
-        <img
-          key="nextPokeDetailBtn"
-          :class="['arrow-btn right', { disabled: nextBtnDisabled }]"
-          :src="assets.arrowIcon"
-          alt="arrow-icon"
-          @click="onArrowBtnClick('next')"
-        />
+        <img key="nextPokeDetailBtn" :class="['arrow-btn right', { disabled: nextBtnDisabled }]" :src="assets.arrowIcon"
+          alt="arrow-icon" @click="onArrowBtnClick('next')" />
       </div>
     </template>
     <div v-else class="pokemon">{{ $t('common.loading') }}</div>
@@ -171,13 +146,13 @@ export default {
     ability() {
       return this.abilities.desc && this.abilities.label
         ? {
-            desc: this.abilities.desc.filter(
-              (item) => languageEnum[item.language.name] === this.$i18n.locale
-            )[0]?.['flavor_text'],
-            label: this.abilities.label.filter(
-              (item) => languageEnum[item.language.name] === this.$i18n.locale
-            )[0]?.name,
-          }
+          desc: this.abilities.desc.filter(
+            (item) => languageEnum[item.language.name] === this.$i18n.locale
+          )[0]?.['flavor_text'],
+          label: this.abilities.label.filter(
+            (item) => languageEnum[item.language.name] === this.$i18n.locale
+          )[0]?.name,
+        }
         : {};
     },
     states() {
@@ -196,6 +171,7 @@ export default {
   methods: {
     ...mapActions('pokemon', ['getPokemonDetail']),
     getData() {
+      this.id = String(this.$route.query.id);
       this.loading = true;
       this.isStatesBarReady = false;
       clearTimeout(this.statesBarReadyTimerId);
@@ -216,7 +192,6 @@ export default {
     goToOtherPokeDetail(btnType) {
       const { $router, id } = this;
       const newId = btnType === 'pre' ? Number(id) - 1 : Number(id) + 1;
-      this.id = String(newId);
       $router.push(`${routerPath.DETAIL}?id=${newId}`);
     },
     onArrowBtnClick(btnType) {
@@ -230,7 +205,7 @@ export default {
   created() {
     // vue router 在 query 更新後由於是同個組件所以不會重新渲染，故監聽 id 變化重新取得資料
     this.$watch(
-      () => this.id,
+      () => this.$route.query.id,
       function () {
         this.getData();
       },
@@ -248,138 +223,162 @@ export default {
 
 .pokedex-detail {
   padding-top: 60px;
+
   .title {
     display: flex;
     justify-content: center;
     align-items: center;
+
     .name {
       font-size: 20px;
       font-weight: 500;
       letter-spacing: 1px;
       margin-right: 10px;
       color: #464646;
+
       &:first-letter {
         text-transform: uppercase;
       }
     }
+
     .id {
       font-size: 16px;
       color: #aaa;
     }
   }
+
   .pokemon {
     display: flex;
     justify-content: center;
     align-items: center;
     margin-top: 20px;
+
     &.fire {
       .img-container {
         background-color: lighten($color: $fireTypeColor, $amount: 30%);
         border: 3px solid $fireTypeColor;
       }
     }
+
     &.grass {
       .img-container {
         background-color: lighten($color: $grassTypeColor, $amount: 40%);
         border: 3px solid $grassTypeColor;
       }
     }
+
     &.electric {
       .img-container {
         background-color: lighten($color: $electricTypeColor, $amount: 30%);
         border: 3px solid $electricTypeColor;
       }
     }
+
     &.water {
       .img-container {
         background-color: lighten($color: $waterTypeColor, $amount: 30%);
         border: 3px solid $waterTypeColor;
       }
     }
+
     &.ground {
       .img-container {
         background-color: lighten($color: $groundTypeColor, $amount: 10%);
         border: 3px solid $groundTypeColor;
       }
     }
+
     &.rock {
       .img-container {
         background-color: lighten($color: $rockTypeColor, $amount: 30%);
         border: 3px solid $rockTypeColor;
       }
     }
+
     &.fairy {
       .img-container {
         background-color: lighten($color: $fairyTypeColor, $amount: 10%);
         border: 3px solid $fairyTypeColor;
       }
     }
+
     &.poison {
       .img-container {
         background-color: lighten($color: $poisonTypeColor, $amount: 10%);
         border: 3px solid $poisonTypeColor;
       }
     }
+
     &.bug {
       .img-container {
         background-color: lighten($color: $bugTypeColor, $amount: 30%);
         border: 3px solid $bugTypeColor;
       }
     }
+
     &.dragon {
       .img-container {
         background-color: lighten($color: $dragonTypeColor, $amount: 10%);
         border: 3px solid $dragonTypeColor;
       }
     }
+
     &.psychic {
       .img-container {
         background-color: lighten($color: $psychicTypeColor, $amount: 10%);
         border: 3px solid $psychicTypeColor;
       }
     }
+
     &.flying {
       .img-container {
         background-color: lighten($color: $flyingTypeColor, $amount: 10%);
         border: 3px solid $flyingTypeColor;
       }
     }
+
     &.fighting {
       .img-container {
         background-color: lighten($color: $fightingTypeColor, $amount: 30%);
         border: 3px solid $fightingTypeColor;
       }
     }
+
     &.normal {
       .img-container {
         background-color: lighten($color: $normalTypeColor, $amount: 30%);
         border: 3px solid $normalTypeColor;
       }
     }
+
     &.ice {
       .img-container {
         background-color: lighten($color: $iceTypeColor, $amount: 30%);
         border: 3px solid $iceTypeColor;
       }
     }
+
     &.ghost {
       .img-container {
         background-color: lighten($color: $ghostTypeColor, $amount: 10%);
         border: 3px solid $ghostTypeColor;
       }
     }
+
     &.steel {
       .img-container {
         background-color: lighten($color: $steelTypeColor, $amount: 10%);
         border: 3px solid $steelTypeColor;
       }
     }
+
     &.dark {
       .img-container {
         background-color: lighten($color: $darkTypeColor, $amount: 30%);
         border: 3px solid $darkTypeColor;
       }
     }
+
     .data-box {
       width: 300px;
       min-height: 450px;
@@ -389,6 +388,7 @@ export default {
       padding: 20px 15px;
       background-color: rgba(255, 255, 255, 0.3);
       box-shadow: 3px 3px 10px #ccc;
+
       .block {
         width: 100%;
         margin-bottom: 20px;
@@ -396,9 +396,11 @@ export default {
         justify-content: center;
         align-items: center;
         position: relative;
+
         .save-my-poke-btn {
           top: -10px;
         }
+
         .img-container {
           width: 150px;
           height: 150px;
@@ -406,6 +408,7 @@ export default {
           display: flex;
           justify-content: center;
           align-items: center;
+
           .poke-photo {
             width: 90%;
             height: 90%;
@@ -413,24 +416,30 @@ export default {
           }
         }
       }
+
       .row {
         display: flex;
         margin-bottom: 10px;
+
         .col-1-2 {
           width: 45%;
         }
+
         .col-1-1 {
           width: 100%;
         }
       }
+
       .label {
         font-size: 15px;
         font-weight: 500;
         color: rgb(25, 10, 155, 0.8);
+
         &:first-letter {
           text-transform: uppercase;
         }
       }
+
       .content {
         width: 90%;
         margin: 8px 0 8px 10px;
@@ -439,22 +448,26 @@ export default {
         font-size: 14px;
         color: #555;
         white-space: nowrap;
+
         &.desc {
           white-space: normal;
           line-height: 1.5;
           text-align: justify;
         }
+
         .state {
           width: 100%;
           display: flex;
           align-items: center;
           margin: 5px 0;
+
           .label {
             font-size: 12px;
             width: 40%;
             display: flex;
             align-items: center;
           }
+
           .value {
             position: relative;
             font-size: 10px;
@@ -469,9 +482,11 @@ export default {
             align-items: center;
             padding: 3px 0;
             background-color: transparent;
+
             span {
               z-index: 1;
             }
+
             .bar {
               position: absolute;
               left: 0;
@@ -481,68 +496,86 @@ export default {
               box-sizing: border-box;
               width: 0;
               transition: width 0.5s;
+
               &.full {
                 border-radius: inherit;
               }
             }
           }
+
           &:nth-child(1) .bar {
             background-color: #fd5959;
           }
+
           &:nth-child(2) .bar {
             background-color: #ffb641;
           }
+
           &:nth-child(3) .bar {
             background-color: #35be16;
           }
+
           &:nth-child(4) .bar {
             background-color: #03b2f7;
           }
+
           &:nth-child(5) .bar {
             background-color: #4c89de;
           }
+
           &:nth-child(6) .bar {
             background-color: #8943de;
           }
         }
       }
     }
+
     .arrow-btn {
       width: 35px;
       height: 35px;
       filter: invert(0.6);
       cursor: pointer;
+
       &:hover:not(.disabled) {
         filter: invert(0.3);
       }
+
       &.left {
         margin-right: 20px;
         transform: rotateY(180deg);
       }
+
       &.right {
         margin-left: 20px;
       }
+
       &.disabled {
         cursor: not-allowed;
       }
     }
   }
 }
+
 @media only screen and (max-width: 530px) {
   .pokedex-detail {
     position: relative;
+
     .pokemon {
       flex-wrap: wrap;
+
       .arrow-btn {
         position: absolute;
         top: 45vh;
+
         &.left {
           left: 3px;
         }
+
         &.right {
           right: 3px;
         }
       }
+
       .data-box {
         &:nth-child(3) {
           margin-bottom: 50px;
