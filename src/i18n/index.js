ee47d9e787843fbceaf954ec2ed072e01c79de50
lthong <appyegde58965@gmail.com>
2023-10-22 12:28:21 +0800

    set the default locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n';
 import enUS from './en-US';
 import zhTW from './zh-TW';
 
-const locale = 'en';
+const locale = 'zh-TW';
 
 export default createI18n({
   locale,
